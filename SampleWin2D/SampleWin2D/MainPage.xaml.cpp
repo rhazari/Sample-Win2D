b373d4644bc8108a2b70817e11c7dc7df454afd2
rhazarik <rhazarik@adobe.com>
2021-02-16 12:35:42 -0600

    init a sample Win2D app

---
+++ SampleWin2D/SampleWin2D/MainPage.xaml.cpp
@@ -0,0 +1,66 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include <ppl.h>
+#include <ppltasks.h>
+#include <agents.h>
+#include <iostream>
+
+using namespace concurrency;
+using namespace std;
+using namespace SampleWin2D;
+
+using namespace Platform;
+using namespace Windows::UI;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+
+using namespace Microsoft::Graphics::Canvas::UI::Xaml;
+using namespace Microsoft::Graphics::Canvas::Geometry;
+using namespace Windows::System::Threading;
+using namespace Windows::Foundation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+void SampleWin2D::MainPage::Click_Me(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e) {
+    control_ = ref new CanvasVirtualControl();
+	control_->RegionsInvalidated += ref new Windows::Foundation::TypedEventHandler<Microsoft::Graphics::Canvas::UI::Xaml::CanvasVirtualControl^, Microsoft::Graphics::Canvas::UI::Xaml::CanvasRegionsInvalidatedEventArgs^>(this, &SampleWin2D::MainPage::OnRegionsInvalidated);
+	//control_->SizeChanged += ref new Windows::UI::Xaml::SizeChangedEventHandler(this, &SampleWin2D::MainPage::OnSizeChanged);
+    
+    // Create a single-shot timer
+    // https://docs.microsoft.com/en-us/windows/uwp/threading-async/use-a-timer-to-submit-a-work-item
+    TimeSpan delay;
+    delay.Duration = 1000000000 / 20;     // Approx about 5 secs
+    ThreadPoolTimer^ delayTimer = ThreadPoolTimer::CreateTimer(ref new TimerElapsedHandler([this](ThreadPoolTimer^) {
+        Dispatcher->RunAsync(CoreDispatcherPriority::High, ref new DispatchedHandler([this]() {
+            control_->Width = 1000;
+            control_->Height = 1000;
+            MyCanvas->Children->Append(control_);
+            MyCanvas->SetLeft(control_, 0);
+            MyCanvas->SetTop(control_, 0);
+        }));
+    }), delay);
+}
+
+//void SampleWin2D::MainPage::OnSizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventHandler^ args) {
+//	control_->Invalidate();
+//}
+
+void SampleWin2D::MainPage::OnRegionsInvalidated(Microsoft::Graphics::Canvas::UI::Xaml::CanvasVirtualControl^ sender, Microsoft::Graphics::Canvas::UI::Xaml::CanvasRegionsInvalidatedEventArgs^ args) {
+	auto rectangle = CanvasGeometry::CreateRectangle(sender, 10, 10, 100, 100);
+	for (const auto& region : args->InvalidatedRegions) {
+		if (auto drawingSession = sender->CreateDrawingSession(region)) {
+			drawingSession->FillGeometry(rectangle, Colors::Bisque);
+		}
+	}
+}
---
+++ SampleWin2D/SampleWin2D/MainPage.xaml.h
@@ -0,0 +1,27 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace SampleWin2D
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void Click_Me(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		//void OnSizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventHandler^ e);
+		void OnRegionsInvalidated(Microsoft::Graphics::Canvas::UI::Xaml::CanvasVirtualControl^ sender, Microsoft::Graphics::Canvas::UI::Xaml::CanvasRegionsInvalidatedEventArgs^ args);
+		
+		Microsoft::Graphics::Canvas::UI::Xaml::CanvasVirtualControl^ control_;
+	};
+}
